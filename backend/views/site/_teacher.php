3fd2a261d72bb373841b39e893a8bb9207f02dbf
Sardorbek200 <yoldashevsardorbek@gmail.com>
2022-12-13 16:12:51 +0500

    new

--- backend/views/site/_teacher.php
+++ backend/views/site/_teacher.php
@@ -2,11 +2,12 @@
 
 use yii\bootstrap5\Html;
 use yii\helpers\Url;
+use yii\helpers\VarDumper;
 
-$img = ($model->teacherInfo->image != '') ? $model->teacherInfo->image : $model->photo;
+$img = ($model->teacherInfo == null) ? $model->photo : $model->teacherInfo->image;
 ?>
 <div class="card p-2">
-    <img class="" src="<?= Yii::getAlias('@defaultImage') . '/' . $img ?>" alt="Card image">
+    <img class="" src="<?= Yii::getAlias('@defaultImage') . '/' . $img ?>" style="height:300px;weight:220px;">
     <div class="card-body">
         <h4 class="h4"><?= $model->first_name . ' ' . $model->last_name ?></h4><br>
         <?= Html::a('To\'liq malumot', Url::to(['user', 'id' => $model->id]), ['class' => 'btn btn-primary']) ?>
