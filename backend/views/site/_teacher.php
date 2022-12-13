09a6f83499d8d179e81e474cd75d98dd740f27c4
Sardorbek200 <yoldashevsardorbek@gmail.com>
2022-12-13 15:29:16 +0500

    teacher

--- backend/views/site/_teacher.php
+++ backend/views/site/_teacher.php
@@ -3,10 +3,10 @@
 use yii\bootstrap5\Html;
 use yii\helpers\Url;
 
-
+$img = ($model->teacherInfo->image != '') ? $model->teacherInfo->image : 'user.png';
 ?>
 <div class="card p-2">
-    <img class="" src="<?= Yii::getAlias('@defaultImage') . '/' . $model->teacherInfo->image ?>" alt="Card image">
+    <img class="" src="<?= Yii::getAlias('@defaultImage') . '/' . $img ?>" alt="Card image">
     <div class="card-body">
         <h4 class="h4"><?= $model->first_name . ' ' . $model->last_name ?></h4><br>
         <?= Html::a('To\'liq malumot', Url::to(['user', 'id' => $model->id]), ['class' => 'btn btn-primary']) ?>
